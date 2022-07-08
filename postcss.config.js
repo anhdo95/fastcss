383ba07cb95a3b3b4381fe904378730062f4372d
Anh Do <do.dinh.anh@linecorp.com>
2022-07-08 09:53:29 +0700

    Move the utilities package to root

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    require('cssnano'),
-    require('autoprefixer'),
-    // require('postcss-modules'),
-    require('postcss-preset-env'),
-    require('./plugins/aspect-ratio'),
-  ]
-}
