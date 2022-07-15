11d07170569d7ce7f30ebf64b552e7fdd32643c1
Anh Do <do.dinh.anh@linecorp.com>
2022-07-15 11:15:18 +0700

    Remove duplicate display

--- src/plugins/flex.js
+++ src/plugins/flex.js
@@ -2,12 +2,6 @@ export default function () {
   return function flex({ addUtilities, variants }) {
     addUtilities(
       {
-        '.flex': {
-          display: 'flex',
-        },
-        '.inline-flex': {
-          display: 'inline-flex',
-        },
         '.flex-row': {
           'flex-direction': 'row',
         },
