9ae1a46a37635d7610d310ae9b04c590bad0b947
Anh Do <do.dinh.anh@linecorp.com>
2022-07-18 07:59:54 +0700

    Integrate fastcss with webpack4

---
+++ integrations/webpack4/fast.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  prefix: 'tw-',
+
+  theme: {
+    extend: {
+    },
+  },
+
+  plugins: [
+    function customUtilities ({ addUtilities, addComponents }) {
+      addComponents({
+        '.card': {
+          'min-width': '33.33333%',
+          'max-width': '50%',
+          'margin-top': '0.5rem',
+          'margin-bottom': '0.5rem',
+          'padding-top': '0.5rem',
+          'padding-bottom': '0.5rem',
+        }
+      })
+
+      addUtilities(
+        {
+          '.flex-center': {
+            display: 'flex',
+            'justify-content': 'center',
+            'align-items': 'center',
+          },
+        },
+        ['responsive']
+      )
+    },
+  ],
+
+  variants: {
+  },
+}
+
