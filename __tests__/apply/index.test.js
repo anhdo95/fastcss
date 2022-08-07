19f56982ffca55f2fc3e509633928ef7b679c8fa
Anh Do <do.dinh.anh@linecorp.com>
2022-08-07 10:59:12 +0700

    Test @apply unknown utility

--- __tests__/apply/index.test.js
+++ __tests__/apply/index.test.js
@@ -3,14 +3,17 @@ import path from 'path'
 import postcss from 'postcss'
 import fast from '../..'
 
+const cssPath = path.resolve(__dirname, 'index.test.css')
+const htmlPath = path.resolve(__dirname, 'index.test.html')
+
 function run(css, config = {}) {
   return postcss([fast(config)]).process(css, { from: '' })
 }
 
 describe('@apply', () => {
-  test.only('@apply matched utilities', () => {
+  test('@apply matched utilities', () => {
     const config = {
-      purge: [path.resolve(__dirname, './index.test.html')],
+      purge: [htmlPath],
       corePlugins: {
         preflight: false,
       },
@@ -34,13 +37,31 @@ describe('@apply', () => {
       }
     `
 
-    return run(css, config)
-      .then((result) => {
-        const expected = fs.readFileSync(
-          path.resolve(__dirname, 'index.test.css'),
-          'utf-8'
-        )
-        expect(result.css).toMatchCss(expected)
-      })
+    return run(css, config).then((result) => {
+      const expected = fs.readFileSync(cssPath, 'utf-8')
+      expect(result.css).toMatchCss(expected)
+    })
+  })
+
+  test('@apply error with unknown utility', async () => {
+    const config = {
+      purge: [htmlPath],
+      corePlugins: {
+        preflight: false,
+      },
+    }
+
+    const css = `
+      @fast components;
+      @fast utilities;
+
+      .card {
+        @apply unknown-utility;
+      }
+    `
+
+    await expect(run(css, config)).rejects.toThrowError(
+      'The `unknown-utility` does not exist.'
+    )
   })
 })
